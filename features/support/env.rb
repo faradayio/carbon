f9180b181ddf5bae6155a50091503021e4b3fe90
Derek Kastner <dkastner@gmail.com>
2012-09-06 16:41:23 -0500

    Use VCR in cucumber shell tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,8 +4,10 @@ require 'fileutils'
 $LOAD_PATH.unshift File.expand_path('../../../lib', __FILE__)
 require 'carbon/shell'
 
+ENV['VCR_CASSETTES'] = File.expand_path('../cassettes', '__FILE__')
+
 Before do
   @aruba_io_wait_seconds = 2
-  @aruba_timeout_seconds = 5
+  @aruba_timeout_seconds = 50
   @dirs = [File.join(ENV['HOME'], 'carbon_features')]
 end
